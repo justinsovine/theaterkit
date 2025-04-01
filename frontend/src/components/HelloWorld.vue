690effd14b42294548e32254ecaf4f3ad02692eb
Justin Sovine <justinsovine@gmail.com>
2025-04-01 14:16:38 -0400

    Re-configured frontend to work with Tailwind

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -9,14 +9,10 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 class="font-extrabold text-large">{{ msg }}</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
   </div>
 
   <p>
